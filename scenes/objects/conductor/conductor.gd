27a58232f774140d28a44d494c837c91e122a9a9
ary <aryan@jha.io>
2024-04-10 16:59:57 -0700

    add metronome

--- scenes/objects/conductor/conductor.gd
+++ scenes/objects/conductor/conductor.gd
@@ -1,12 +1,21 @@
 extends AudioStreamPlayer3D
 
-var bpm: float = 120
-var time: float = 0
+# song metadata
+var bpm: float = stream.bpm if stream.bpm != 0 else 120 # beats per minute
+var spb: float = 60.0 / bpm # seconds per beat
+
+# runtime information
+var current_beat: int = 0
 
 func _ready():
 	play()
 
 func _process(delta):
-	time -= (get_playback_position()
+	var time = (get_playback_position()
 		+ AudioServer.get_time_since_last_mix()
 		- AudioServer.get_output_latency())
+	
+	var new_beat = int(time / spb)
+	if current_beat != new_beat:
+		current_beat = new_beat
+		$metronome.play()
