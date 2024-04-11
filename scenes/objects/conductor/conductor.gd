cd6a04eaa8cedc5807ae55e04375a7a8a99fb25b
ary <aryan@jha.io>
2024-04-11 15:12:56 -0700

    tick on lines and beats

--- scenes/objects/conductor/conductor.gd
+++ scenes/objects/conductor/conductor.gd
@@ -6,16 +6,34 @@ var spb: float = 60.0 / bpm # seconds per beat
 
 # runtime information
 var current_beat: int = 0
+var current_line: int = 0
+
+# conductor configuration
+@export_group("Tools")
+@export var metronome: bool = false ## Play a tick every beat
+
+@export_group("Configuration")
+@export var lines_per_beat: int = 2 ## Number of programmable lines per beat
 
 func _ready():
 	play()
 
 func _process(delta):
-	var time = (get_playback_position()
+	var time: float = (get_playback_position()
 		+ AudioServer.get_time_since_last_mix()
 		- AudioServer.get_output_latency())
 	
-	var new_beat = int(time / spb)
+	var new_beat: int = int(time / spb)
+	var new_line: int = int(time / spb * lines_per_beat)
+	if current_line != new_line:
+		current_line = new_line
+		line()
 	if current_beat != new_beat:
 		current_beat = new_beat
-		$metronome.play()
+		beat()
+
+func beat():
+	if metronome: $metronome.play()
+
+func line():
+	$CSGSphere3D.visible = !$CSGSphere3D.visible
