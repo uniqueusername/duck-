3f43885cbb3db761eee9fd4d1e88a9529ffec8aa
ary <aryan@jha.io>
2024-04-16 16:37:50 -0700

    add sky

--- scenes/levels/base_map/hit_indicator.gd
+++ scenes/levels/base_map/hit_indicator.gd
@@ -5,10 +5,10 @@ extends Node3D
 func _ready():
 	spawn_indicator()
 
-func spawn_indicator():
+func spawn_indicator():	
 	for i in range(map.lane_count):
 		var lane = CSGBox3D.new()
-		lane.size = Vector3(2 * sin(PI / map.lane_count), 0.1, 0.1)
+		lane.size = Vector3(2.35 * sin(PI / map.lane_count), 0.05, 0.05)
 		lane.rotation.z = i * (2 * PI / map.lane_count)
 		lane.position.y = -1
 		lane.position = lane.position.rotated(Vector3.BACK, i * (2 * PI / map.lane_count))
