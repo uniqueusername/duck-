16e8a7f4e41acee5af87d8e3738b81acc66da2a2
ary <aryan@jha.io>
2024-04-16 14:50:11 -0700

    expand seconds_per_beat variable name for clarity

--- scenes/objects/conductor/conductor.gd
+++ scenes/objects/conductor/conductor.gd
@@ -2,7 +2,7 @@ extends AudioStreamPlayer3D
 
 # song metadata
 var bpm: float = stream.bpm if stream.bpm != 0 else 120 # beats per minute
-var spb: float = 60.0 / bpm # seconds per beat
+var seconds_per_beat: float = 60.0 / bpm # seconds per beat
 
 # runtime information
 var current_beat: int = 0
@@ -23,8 +23,8 @@ func _process(delta):
 		+ AudioServer.get_time_since_last_mix()
 		- AudioServer.get_output_latency())
 	
-	var new_beat: int = int(time / spb)
-	var new_line: int = int(time / spb * lines_per_beat)
+	var new_beat: int = int(time / seconds_per_beat)
+	var new_line: int = int(time / seconds_per_beat * lines_per_beat)
 	if current_line != new_line:
 		current_line = new_line
 		line()
