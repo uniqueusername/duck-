f467b38932d8509fa2d76a7925bd7f83c6fa6aa7
ary <aryan@jha.io>
2024-04-17 17:00:55 -0700

    add accuracy system

--- scenes/levels/base_map/notes.gd
+++ scenes/levels/base_map/notes.gd
@@ -3,6 +3,7 @@ extends Node3D
 # references
 @onready var map: DuckMap = %conductor.map
 var note_scene: PackedScene = preload("res://scenes/objects/note/note.tscn")
+var exclamation_scene: PackedScene = preload("res://scenes/objects/accuracy_exclamation/accuracy_exclamation.tscn")
 var lanes = []
 var tween: Tween
 
@@ -15,17 +16,40 @@ var focused_lane: int = 0: # 0-indexed, circular
 		else: focused_lane = value
 @onready var lines_per_second: float = map.lines_per_beat / %conductor.seconds_per_beat
 @onready var active_lanes_array = map.get_active_lanes_array()
-var health: float = 100
+
+# gameplay stuff
+var health: float = 100:
+	set(value):
+		if value > 100: health = 100
+		else: health = value
+		$health.text = str(health)
+var accuracy_margin: float = 0.2 # how many seconds before a note is missed
+@onready var listener_starts = map.lane_changes
+var listeners = []
 
 func _ready():
 	create_lanes()
 	spawn_notes()
+	config_listeners()
 	
 	%conductor._line.connect(sync_notes)
 	%conductor._line.connect(check_active_lane)
 
 func _process(delta):
 	$lanes.position.z += lines_per_second * delta
+	
+	if listener_starts.size() > 0 and %conductor.time > listener_starts[0]:
+		listeners.append(accuracy_margin)
+		listener_starts.remove_at(0)
+		
+	for i in range(listeners.size()):
+		listeners[i] -= delta
+		if listeners[i] < -accuracy_margin:
+			listeners.remove_at(i)
+			var exclamation = exclamation_scene.instantiate()
+			exclamation.text = "miss"
+			add_child(exclamation)
+			health -= 25
 
 # configure rotation markers and display indicator
 func create_lanes():
@@ -55,8 +79,11 @@ func sync_notes(current_line: int):
 func check_active_lane(current_line: int):
 	var check_lane: int = 0 if focused_lane == 0 else 6 - focused_lane
 	if not active_lanes_array[current_line].has(check_lane):
-		health -= 2
-		$Label.text = str(health)
+		health -= 5
+
+func config_listeners():
+	for i in range(listener_starts.size()):
+		listener_starts[i] -= accuracy_margin
 
 # set up tween for rotation
 func do_rotation(angle: float):
@@ -67,10 +94,22 @@ func do_rotation(angle: float):
 func _input(event):
 	if event.is_action_pressed("left"):
 		focused_lane += 1
-		do_rotation(lane_rotations[focused_lane])
 	if event.is_action_pressed("right"):
 		focused_lane -= 1
-		do_rotation(lane_rotations[focused_lane])
 	if event.is_action_pressed("flip"):
 		focused_lane += 3
-		do_rotation(lane_rotations[focused_lane])
+	
+	if listeners.size() > 0:
+		var accuracy = listeners[0]
+		listeners.remove_at(0)
+		
+		var exclamation = exclamation_scene.instantiate()
+		if abs(accuracy) < 0.25 * accuracy_margin:
+			exclamation.text = "perfect"
+			health += 10
+		else:
+			exclamation.text = "bad"
+			health += 3
+		add_child(exclamation)
+		
+	do_rotation(lane_rotations[focused_lane])
